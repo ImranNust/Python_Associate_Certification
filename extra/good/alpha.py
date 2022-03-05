94c5236ee0529dac08d7534687d1a39536fee857
Imran <imran.muet@gmail.com>
2022-03-05 18:24:33 +0500

    Deleted unnecessay files.

--- extra/good/alpha.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-""" example module: extra.good.alpha """
-
-def FunA():
-	return "Alpha"
-
-if __name__ == "__main__":
-	print("I prefer to be a module")
--- extra/good/best/tau.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-""" example module: extra.good.best.tau """
-
-def FunT():
-	return "Tau"
-
-if __name__ == "__main__":
-	print("I prefer to be a module")
